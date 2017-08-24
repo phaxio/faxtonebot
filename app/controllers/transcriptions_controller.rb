0dbe2dc502e962c6289ad10ff137596c6a334320
Joshua Nankin <josh@phaxio.com>
2017-08-24 11:54:50 -0500

    Revert "Set result to [Fax Tone Detected] if no text is received"
    This reverts commit a52d938e729843e0b9b7b32f6b1200c60ecb1e29.

--- app/controllers/transcriptions_controller.rb
+++ app/controllers/transcriptions_controller.rb
@@ -6,10 +6,23 @@ class TranscriptionsController < ApplicationController
 
     text = params['TranscriptionText']
     if text.strip.present?
-      tone_check.note = "#{tone_check.note}\n\nTranscript: \"#{text}\"".strip
-      tone_check.result = "#{tone_check.result} [Voice Detected]".strip
+      tone_check.note = if tone_check.note.present?
+        tone_check.note = "#{tone_check.note}\n\nTranscript: \"#{text}\""
+      else
+        text
+      end
+
+      tone_check.result = if tone_check.result.present?
+        "#{tone_check.result}/Voice Detected"
+      else
+        'Voice Detected'
+      end
     else
-      tone_check.result = "#{tone_check.result} [Fax Tone Detected]".strip
+      tone_check.note = if tone_check.note.present?
+        "#{tone_check.note}\n\nNo Voice Detected"
+      else
+        'No Voice Detected'
+      end
     end
 
     tone_check.save!
