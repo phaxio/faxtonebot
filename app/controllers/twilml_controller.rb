df96c5155d23758faa5a6854d2146216e52d2df5
Josh Nankin <josh@phaxio.com>
2019-03-26 15:46:46 -0500

    fixing again

--- app/controllers/twilml_controller.rb
+++ app/controllers/twilml_controller.rb
@@ -2,9 +2,8 @@ class TwilmlController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def pause
-    response = Twilio::TwiML::VoiceResponse.new do |r|
-      r.Pause '', length: 30
-    end
+    response = Twilio::TwiML::VoiceResponse
+    response.pause(length: 30)
 
     render text: response.to_s
   end
